24045ad26de8f42c5ef3a22d3efc0fc2c0051f97
Mauro Pastor Anduix <mauropasan01@gmail.com>
2022-09-19 12:46:32 +0200

    [ADD] act1.2

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,82 @@
+/**
+ * @author mauropasan
+ */
 'use strict'
 
+/**
+ * 
+ * @param {cadena} cadena String
+ * @returns total de carácteres
+ */
 function letras(cadena) {
-
+	return cadena.length;
 }
 
+/**
+ * 
+ * @param {cadena} cadena String
+ * @returns total de palabras
+ */
 function palabras(cadena) {
-
+	let words = cadena.split(' ');
+	return words.length;
 }
 
+/**
+ * 
+ * @param {cadena} cadena String
+ * @returns cadena en mayúscula
+ */
 function maysc(cadena) {
-
+	return cadena.toLocaleUpperCase();
 }
 
+/**
+ * 
+ * @param {cadena} cadena String
+ * @returns cadena con cada primera letra de cada palabra en mayúscula
+ */
 function titulo(cadena) {
-
+	let cadenaSplit = cadena.split(' ');
+	for (let word in cadenaSplit) {
+		// cadenaSplit va cogiendo un word dentro de cadenaSplit, luego el primer carácter de word y lo pone en mayúscula para luego juntarlo con el resto de word
+		cadenaSplit[word] = cadenaSplit[word].charAt(0).toLocaleUpperCase() + cadenaSplit[word].substring(1);
+	}
+	return cadenaSplit.join(' ');
 }
 
+/**
+ * 
+ * @param {cadena} cadena String
+ * @returns cadena invertida
+ */
 function letrasReves(cadena) {
-
+	//Usa toda la cadena y la invierte sin usar ningún delimitador
+	return cadena.split('').reverse().join('');
 }
 
+/**
+ * 
+ * @param {cadena} cadena String
+ * @returns cadena con las palabras invertidas
+ */
 function palabrasReves(cadena) {
-
+	// Usa toda la cadena e invierte el órden de las palabras usando el delimitador ' ' (espacio)
+	return cadena.split(' ').reverse().join(' ');
 }
 
 function palindromo(cadena) {
-
+	let inversedString = palabrasReves(cadena);
+	let invertedNoSpaces = inversedString.replace(/\s/g, '');
+	let noSpaces = cadena.replace(/\s/g, '');
+	let result = noSpaces.toLocaleLowerCase;
+	let invertedResult = invertedNoSpaces.toLocaleLowerCase;
+	if(result === invertedResult) {
+		console.log('Sí es un palíndromo');
+	} else {
+		console.log('No es un palíndromo');
+	}
+	return result === invertedResult;
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,59 @@
+/**
+ * @author mauropasan
+ */
 'use strict'
 
+let phrase = prompt('Escriu una frase');
 
+function letras() {
+	let chars = phrase.length;
+	console.log(chars);
+	return chars;
+}
+
+function palabras() {
+	let words = phrase.split(' ');
+	let wordsNum = words.length;
+	console.log(wordsNum);
+	return wordsNum;
+}
+
+function maysc() {
+    let uppercasePhrase = phrase.toLocaleUpperCase();
+	console.log(uppercasePhrase);
+	return uppercasePhrase;
+}
+
+function titulo() {
+	let phraseSplit = phrase.split(' ');
+	for (let word in phraseSplit) {
+		// cadenaSplit va cogiendo un word dentro de cadenaSplit, luego el primer carácter de word y lo pone en mayúscula para luego juntarlo con el resto de word
+		phraseSplit[word] = phraseSplit[word].charAt(0).toLocaleUpperCase() + phraseSplit[word].substring(1);
+	}
+	let result = phraseSplit.join(' ');
+	console.log(result);
+	return result;
+}
+
+function letrasReves() {
+	//Usa toda la cadena y la invierte sin usar ningún delimitador
+	let invertedString = phrase.split('').reverse().join('');
+	console.log(invertedString);
+	return invertedString;
+}
+
+function palabrasReves() {
+	// Usa toda la cadena e invierte el órden de las palabras usando el delimitador ' ' (espacio)
+	let invertedWordsString = phrase.split(' ').reverse().join(' ');
+	console.log(invertedWordsString);
+	return invertedWordsString;
+}
+
+function palindromo() {
+	let inversedString = palabrasReves(cadena);
+	let invertedNoSpaces = inversedString.replace(/\s/g, '');
+	let noSpaces = cadena.replace(/\s/g, '');
+	let result = noSpaces.toLocaleLowerCase;
+	let invertedResult = invertedNoSpaces.toLocaleLowerCase;
+	return result === invertedResult;
+}
