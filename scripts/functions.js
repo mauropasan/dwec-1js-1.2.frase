afd82f1d0f73b17d5071429f803437921069a404
Mauro Pastor Anduix <mauropasan01@gmail.com>
2022-09-22 22:35:02 +0200

    [ADD] act 1.2

--- scripts/functions.js
+++ scripts/functions.js
@@ -37,12 +37,10 @@ function maysc(cadena) {
  * @returns cadena con cada primera letra de cada palabra en mayúscula
  */
 function titulo(cadena) {
-	let cadenaSplit = cadena.split(' ');
-	for (let word in cadenaSplit) {
-		// cadenaSplit va cogiendo un word dentro de cadenaSplit, luego el primer carácter de word y lo pone en mayúscula para luego juntarlo con el resto de word
-		cadenaSplit[word] = cadenaSplit[word].charAt(0).toLocaleUpperCase() + cadenaSplit[word].substring(1);
-	}
-	return cadenaSplit.join(' ');
+	return cadena
+	.split(' ')
+	.map(word => word.charAt(0).toLocaleUpperCase() + word.substr(1))
+	.join(' ').trim();
 }
 
 /**
@@ -71,17 +69,8 @@ function palabrasReves(cadena) {
  * @returns true o false si el String es palíndromo
  */
 function palindromo(cadena) {
-	let inversedString = palabrasReves(cadena);
-	let invertedNoSpaces = inversedString.replace(/\s/g, '');
-	let noSpaces = cadena.replace(/\s/g, '');
-	let result = noSpaces.toLocaleLowerCase;
-	let invertedResult = invertedNoSpaces.toLocaleLowerCase;
-	if(result === invertedResult) {
-		console.log('Sí es un palíndromo');
-	} else {
-		console.log('No es un palíndromo');
-	}
-	return result === invertedResult;
+	cadena = cadena.toLowerCase().split(' ').join('');
+	return cadena === letrasReves(cadena);
 }
 
 module.exports = {
